73cd6e1f8da3e7a8e2352f5af858d2fa6d075445
Gospel Chukwunonso <130040013+GospelHack33@users.noreply.github.com>
2023-08-03 21:33:50 +0100

    Add files via upload

---
+++ tmux-install.py
@@ -0,0 +1,7 @@
+# update and install all termux packages for ethical hacking...
+import os
+os.system('clear')
+os.system('apt update && apt upgrade -y && apt install wget -y && apt install python -y && apt install python2 -y && apt install openssh -y && apt install git -y && apt install nmap -y')
+os.system('mkdir tmux-tools && cd tmux-tools && git clone https://github.com/GospelHack33/Ethical-Hacker && git clone https://github.com/GospelHack33/MapSploit && git clone https://github.com/GospelHack33/Pentest && git clone https://github.com/GospelHack33/GospelCoding && git clone https://github.com/GospelHack33/d3S4down && git clone https://github.com/GospelHack33/SearchCVE && git clone https://github.com/GospelHack33/SlowMeDown && git clone https://github.com/GospelHack33/LinPEx && git clone https://github.com/GospelHack33/sh_upgrade && git clone https://github.com/GospelHack33/DarkNei && git clone https://github.com/GospelHack33/SqlDetect && git clone https://github.com/GospelHack33/Spider')
+os.system('clear')
+print('\n[!] tmux-install completed successfully...\n')
